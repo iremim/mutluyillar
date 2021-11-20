6d6831118f610ad1f88d9412715168ddf2a23279
Mehmet-Kaan <taspunar10@hotmail.com>
2021-11-21 00:01:39 +0100

    asd

---
+++ addPic 2.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/e057c972d7.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="stylesheets/addPic.css">
+    <title>Foto Ekle</title>
+</head>
+<body>    
+
+    <a href="index.php" id="aYear"><i class="fa fa-home" style="z-index: 20; font-size:24px;position: absolute;color: white;top: 0.8vh; left: 1.5vw;"></i></a>
+    <a href="ayear.php" id="aYear"><i class="material-icons" style="z-index: 20; font-size:24px;position: absolute;color: white;top: 0.8vh; right: 1.5vw;">collections</i></a>
+    <main>
+        <div id="title">
+            <h1 style="font-family: 'Cuprum', sans-serif;">Albume Ekle</h1>
+        </div>
+        <div id="inputPlace">
+
+        </div>
+    </main>
+
+    <script src="scripts/pictures.js"></script>
+    <script src="scripts/addPic.js"></script>
+</body>
+</html>
